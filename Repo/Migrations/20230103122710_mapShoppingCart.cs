d66bbef81ddb04cb1d6aae6bdbe20d75c6437b9b
Ahmed Gamal <agzaid00@gmail.com>
2023-01-04 10:46:28 +0200

    added mapshoppingcart

---
+++ Repo/Migrations/20230103122710_mapShoppingCart.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repo.Migrations
+{
+    public partial class mapShoppingCart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCart_AspNetUsers_AppUserId",
+                table: "ShoppingCart");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCart_AspNetUsers_AppUserId",
+                table: "ShoppingCart",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCart_AspNetUsers_AppUserId",
+                table: "ShoppingCart");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCart_AspNetUsers_AppUserId",
+                table: "ShoppingCart",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
